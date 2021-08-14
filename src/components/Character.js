c6a34cbbecb6fdb66517a173b0dd4666fd915be9
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-13 18:03:45 -0700

    checking

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+// import React from 'react'
+
+export default function Characters(props) {
+  //   const { actor } = props;
+  const { actor } = props;
+  return (
+    <div>
+      <h2>{actor.name}</h2>
+      <h3>{actor.height}</h3>
+    </div>
+  );
+}
