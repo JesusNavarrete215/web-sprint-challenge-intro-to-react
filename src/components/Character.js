1fc35c6c58c885ade5f4c4c177f236c079d08200
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-13 22:37:11 -0700

    styled

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,27 @@
 // Write your Character component here
-// import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Container } from "reactstrap";
+
+const CharaStyle = styled.h2`
+  border: 2px solid;
+  padding: 1rem;
+  border-radius: 3px;
+  width: 50%;
+  margin-bottom: 20px;
+  margin-left: 25%;
+  background: grey;
+`;
 
 export default function Characters(props) {
   //   const { actor } = props;
   const { actor } = props;
   return (
     <div>
-      <h2>{actor.name}</h2>
-      <h3>{actor.height}</h3>
+      <CharaStyle>
+        {actor.name}
+        {actor.height}
+      </CharaStyle>
     </div>
   );
 }
